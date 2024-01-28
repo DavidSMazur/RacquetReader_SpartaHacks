91a577cfd52b1f9f3203cc5e1fc1c99c6f0c9c16
Nathan Tafelsky <112997769+nwtaf@users.noreply.github.com>
2024-01-28 04:01:02 -0500

    Revert "still testing."
    This reverts commit 4c4c1bc45fc7b8cef74b1af04afcceedfaf917db.

--- src/python/main.py
+++ src/python/main.py
@@ -1,3 +1,5 @@
+# TODO: sometimes the code saves the output video correctly, see print 'VideoWriter released successfully'
+
 from collections import defaultdict
 from pathlib import Path
 import cv2
@@ -13,7 +15,7 @@ file_number = '2'
 model = YOLO('models/yolov8n-pose.pt')
 
 # Define video source
-video_path = str(Path('data/video/swing.mp4'))
+video_path = str(Path('data/video/swing_short.mp4'))
 video_source = video_path
 cap = cv2.VideoCapture(video_source)
 
@@ -42,9 +44,6 @@ out = cv2.VideoWriter(str(output_video_path), cv2.VideoWriter_fourcc(*'vp09'), f
 center_history = defaultdict(lambda: [])
 wrist_history = defaultdict(lambda: [])
 
-# Variable to track if the first object is detected
-first_object_detected = False
-
 # Loop through the video frames
 while cap.isOpened():
     # Read a frame from the video
@@ -60,61 +59,53 @@ while cap.isOpened():
             track_ids = result.boxes.id.int().cpu().tolist()
             keypoints_list = result.keypoints.xy.cpu().tolist()
 
-            # Check if the first object is detected
-            if not first_object_detected:
-                # Set first_object_detected to True
-                first_object_detected = True
+            # For each tracked object, iterate over its keypoints.
+            # If the keypoint is the right wrist (index 10), append its coordinates to the wrist_history for the current track_id.
+            # If the wrist_history for the current track_id exceeds the maximum track length, remove the oldest entry.
+            for track_id, keypoints in zip(track_ids, keypoints_list):
+                for i, keypoint in enumerate(keypoints):
+                    x, y = keypoint[:2]  # Only take the first two values
+                    if i == 10:  # If the keypoint is the right wrist
+                        wrist_history[track_id].append((float(x), float(y)))  # x, y center point
+                        if len(wrist_history[track_id]) > max_track_length:
+                            wrist_history[track_id].pop(0)
 
-                # Continue with processing the detected object
+            # For each bounding box and its corresponding track_id, extract the box's coordinates.
+            # Append the center point of the box (x, y) to the tracking history for the current track_id.
+            # If the tracking history for the current track_id exceeds the maximum track length, remove the oldest entry.
+            for box, track_id in zip(boxes, track_ids):
+                x, y, w, h = box
+                track = center_history[track_id]
+                track.append((float(x), float(y)))  # x, y center point
+                if len(track) > max_track_length:
+                    track.pop(0)
 
-                # For each tracked object, iterate over its keypoints.
-                # If the keypoint is the right wrist (index 10), append its coordinates to the wrist_history for the current track_id.
-                # If the wrist_history for the current track_id exceeds the maximum track length, remove the oldest entry.
-                for track_id, keypoints in zip(track_ids, keypoints_list):
-                    for i, keypoint in enumerate(keypoints):
-                        x, y = keypoint[:2]  # Only take the first two values
-                        if i == 10:  # If the keypoint is the right wrist
-                            wrist_history[track_id].append((float(x), float(y)))  # x, y center point
-                            if len(wrist_history[track_id]) > max_track_length:
-                                wrist_history[track_id].pop(0)
+            # Visualize the results on the frame
+            annotated_frame = result.plot()
 
-                # For each bounding box and its corresponding track_id, extract the box's coordinates.
-                # Append the center point of the box (x, y) to the tracking history for the current track_id.
-                # If the tracking history for the current track_id exceeds the maximum track length, remove the oldest entry.
-                for box, track_id in zip(boxes, track_ids):
-                    x, y, w, h = box
-                    track = center_history[track_id]
-                    track.append((float(x), float(y)))  # x, y center point
-                    if len(track) > max_track_length:
-                        track.pop(0)
+            # Plot the center_history tracking lines
+            for track_id in center_history:
+                points = np.hstack(center_history[track_id]).astype(np.int32).reshape((-1, 1, 2))
+                cv2.polylines(annotated_frame, [points], isClosed=False, color=(230, 230, 230), thickness=10)
 
-                # Visualize the results on the frame
-                annotated_frame = result.plot()
+            # Plot the wrist_history tracking lines
+            for track_id in wrist_history:
+                points = np.hstack(wrist_history[track_id]).astype(np.int32).reshape((-1, 1, 2))
+                cv2.polylines(annotated_frame, [points], isClosed=False, color=(0, 255, 0), thickness=10)
 
-                # Plot the center_history tracking lines
-                for track_id in center_history:
-                    points = np.hstack(center_history[track_id]).astype(np.int32).reshape((-1, 1, 2))
-                    cv2.polylines(annotated_frame, [points], isClosed=False, color=(230, 230, 230), thickness=10)
+            # Resize the annotated frame for display
+            annotated_frame = cv2.resize(annotated_frame, None, fx=scale_factor, fy=scale_factor)
 
-                # Plot the wrist_history tracking lines
-                for track_id in wrist_history:
-                    points = np.hstack(wrist_history[track_id]).astype(np.int32).reshape((-1, 1, 2))
-                    cv2.polylines(annotated_frame, [points], isClosed=False, color=(0, 255, 0), thickness=10)
+            # Write the frame to the output video file
+            out.write(annotated_frame)
+            print(f'writing to {output_video_path}')
 
-                # Resize the annotated frame for display
-                annotated_frame = cv2.resize(annotated_frame, None, fx=scale_factor, fy=scale_factor)
+            # Display the annotated frame
+            cv2.imshow("YOLOv8 Tracking", annotated_frame)
 
-                # Write the frame to the output video file
-                out.write(annotated_frame)
-                print(f'writing to {output_video_path}')
-
-                # Display the annotated frame
-                cv2.imshow("YOLOv8 Tracking", annotated_frame)
-
-                # Break the loop if 'q' is pressed
-                if cv2.waitKey(1) & 0xFF == ord("q"):
-                    break
-
+            # Break the loop if 'q' is pressed
+            if cv2.waitKey(1) & 0xFF == ord("q"):
+                break
     else:
         # Break the loop if the end of the video is reached
         break
