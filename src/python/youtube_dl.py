7773ddb064e6d910f023841d7a0fac714aaf8484
Nathan Tafelsky <112997769+nwtaf@users.noreply.github.com>
2024-01-27 19:16:52 -0500

    reorganized, added flowchart files

--- src/python/youtube_dl.py
+++ src/python/youtube_dl.py
@@ -3,7 +3,9 @@ from pytube import YouTube
 from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
 
 # Specify the YouTube URL
-youtube_url = 'https://www.youtube.com/shorts/G7ZJNaszMrc'
+# youtube_url = 'https://www.youtube.com/shorts/G7ZJNaszMrc'
+youtube_url = 'https://www.youtube.com/watch?v=oOsmGn2_piE'
+
 
 # Create a YouTube object
 yt = YouTube(youtube_url)
@@ -16,11 +18,11 @@ download_path = yt.streams.get_highest_resolution().download(data_dir)
 
 # Optional: specify the start and end times of the segment you want to save (in seconds)
 
-start_time = 0
-end_time = 2
+start_time = 12
+end_time = 16
 
 # Specify the output file for the segment
-output_file = Path(data_dir, 'swing_short.mp4')
+output_file = Path(data_dir, 'serena_swing.mp4')
 
 # Extract the segment and save it
 ffmpeg_extract_subclip(download_path, start_time, end_time, targetname=output_file)
